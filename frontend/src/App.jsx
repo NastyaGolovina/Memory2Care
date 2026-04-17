ab17c22d13c2abd47f5db4cc4d0c1d243638781c
Anastasiia <n21koketka@gmail.com>
2026-04-17 17:16:04 +0300

    fix header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
                     <Navbar />
                     <Content style={{ padding: "24px" }}>
                         <Routes>
-                            <Route path="/home" element={<HomePage  slug={"main_page"}/>} />
+                            <Route path="/" element={<HomePage  slug={"main_page"}/>} />
                             <Route path="/about" element={<AboutPage />} />
                             <Route path="/team-partners" element={<TeamPartnersPage />} />
                             <Route path="/news" element={<NewsPage />} />
